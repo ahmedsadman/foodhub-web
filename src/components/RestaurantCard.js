d2db7ae40bbb6eaa0a34bfe2d3779cf0b9e41caf
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2019-04-05 01:29:42 +0600

    reusable restaurant card component

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Card } from './common/Card';
+
+export const RestaurantCard = props => {
+    return (
+        <Card style={{ marginBottom: 15 }}>
+            <div style={styles.mainContainer}>
+                <img
+                    src='/images/css/Takeout.jpg'
+                    alt=''
+                    style={{ width: 100, height: 100, flexGrow: 1 }}
+                />
+                <div style={styles.basicInfo}>
+                    <span style={{ fontSize: '140%' }}>{props.name}</span>
+                    <span style={{ fontSize: '80%' }}>
+                        17, Level-3, Sonargaon Janapth, Dhaka
+                    </span>
+                    <div style={styles.meta}>
+                        <span style={{ fontSize: '90%' }}>Fast Food</span>
+                        <span style={{ fontSize: '90%', color: 'orange' }}>
+                            Open Now
+                        </span>
+                    </div>
+                </div>
+                <div style={styles.ratingSection}>
+                    <span style={styles.rating}>4.8</span>
+                    <span
+                        style={{
+                            fontSize: '70%',
+                            display: 'block',
+                            marginTop: 5,
+                            textAlign: 'center'
+                        }}
+                    >
+                        778 Reviews
+                    </span>
+                </div>
+                <div style={styles.icons}>
+                    <i className='fa fa-phone' style={styles.icon} />
+                    <i className='fa fa-envelope' style={styles.icon} />
+                    <i className='fa fa-search' style={styles.icon} />
+                </div>
+            </div>
+        </Card>
+    );
+};
+
+const styles = {
+    mainContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        position: 'relative',
+    },
+    basicInfo: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1.5,
+        marginLeft: 15
+    },
+    meta: {
+        marginTop: 15,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    rating: {
+        display: 'block',
+        backgroundColor: '#ED7D31',
+        padding: '6px 8px',
+        alignSelf: 'center',
+        color: '#fff',
+        fontSize: '120%',
+        textAlign: 'center'
+    },
+    ratingSection: {
+        display: 'flex',
+        flexGrow: 1,
+        flexDirection: 'column',
+        marginLeft: 40
+    },
+    icons: {
+        position: 'absolute',
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    icon: {
+        color: 'orange',
+        fontSize: 18,
+        padding: '0 3px'
+    }
+};
--- src/routes/SearchResult.js
+++ src/routes/SearchResult.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { RestaurantCard } from '../components/RestaurantCard';
 import styles from '../views/SearchResult.module.css';
 
 class SearchResult extends Component {
@@ -88,9 +89,16 @@ class SearchResult extends Component {
                         </ul>
                     </div>
                 </nav>
-
-                {/*Filter Block*/}
-                <div id='main' style={{ marginLeft: 'auto', marginRight: 'auto', width: 1140, paddingTop: 30, paddingBottom: 30 }}>
+                <div
+                    id='main'
+                    style={{
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        width: 1140,
+                        paddingTop: 30,
+                        paddingBottom: 30
+                    }}
+                >
                     <section style={{ float: 'left' }}>
                         <div className={styles.filterBlock}>
                             <span className={styles.orange}>
@@ -206,186 +214,12 @@ class SearchResult extends Component {
                     </section>
                     {/*Restaurant Block */}
                     <div className={styles.Restaurants}>
-                        <div className={styles.resBlock}>
-                            <div className={styles.left}>
-                                <img
-                                    alt=''
-                                    className={styles.resImg}
-                                    src='/images/css/Takeout.jpg'
-                                />
-                            </div>
-                            <div className={styles.right}>
-                                <div className={styles.large}>Takeout</div>
-                                <div className={styles.small}>
-                                    17, Level - 3, Sonargaon Janapath, Rd No 3
-                                    <br />
-                                    Uttara, Dhaka.
-                                    <br />
-                                    <br />
-                                </div>
-                                <div className={styles.medium}>
-                                    {' '}
-                                    Fast food
-                                    <br />
-                                    $$$
-                                    <br />
-                                </div>
-                                <div className={styles.orange}>Open now</div>
-                            </div>
-                            <div className={styles.numbers}>
-                                <div className={styles.rating}>4.8</div>
-                                <div className={styles.reviewCheckins}>
-                                    778 Reviews
-                                    <br />
-                                    2458 Check-ins
-                                </div>
-                                <div className={styles.icons}>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-phone`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-envelope`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-search`}
-                                        />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={styles.resBlock}>
-                            <div className={styles.left}>
-                                <img
-                                    alt=''
-                                    className={styles.resImg}
-                                    src='/images/css/Takeout.jpg'
-                                />
-                            </div>
-                            <div className={styles.right}>
-                                <div className={styles.large}>Takeout</div>
-                                <div className={styles.small}>
-                                    17, Level - 3, Sonargaon Janapath, Rd No 3
-                                    <br />
-                                    Uttara, Dhaka.
-                                    <br />
-                                    <br />
-                                </div>
-                                <div className={styles.medium}>
-                                    {' '}
-                                    Fast food
-                                    <br />
-                                    $$$
-                                    <br />
-                                </div>
-                                <div className={styles.orange}>Open now</div>
-                            </div>
-                            <span className={styles.numbers}>
-                                <div className={styles.rating}>4.8</div>
-                                <div className={styles.reviewCheckins}>
-                                    778 Reviews
-                                    <br />
-                                    2458 Check-ins
-                                </div>
-                                <div className={styles.icons}>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-phone`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-envelope`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-search`}
-                                        />
-                                    </Link>
-                                </div>
-                            </span>
-                        </div>
-
-                        <div className={styles.resBlock}>
-                            <div className={styles.left}>
-                                <img
-                                    alt=''
-                                    className={styles.resImg}
-                                    src='/images/css/Takeout.jpg'
-                                />
-                            </div>
-                            <div className={styles.right}>
-                                <div className={styles.large}>Takeout</div>
-                                <div className={styles.small}>
-                                    17, Level - 3, Sonargaon Janapath, Rd No 3
-                                    <br />
-                                    Uttara, Dhaka.
-                                    <br />
-                                    <br />
-                                </div>
-                                <div className={styles.medium}>
-                                    {' '}
-                                    Fast food
-                                    <br />
-                                    $$$
-                                    <br />
-                                </div>
-                                <div className={styles.orange}>Open now</div>
-                            </div>
-                            <span className={styles.numbers}>
-                                <div className={styles.rating}>4.8</div>
-                                <div className={styles.reviewCheckins}>
-                                    778 Reviews
-                                    <br />
-                                    2458 Check-ins
-                                </div>
-                                <div className={styles.icons}>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-phone`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-envelope`}
-                                        />
-                                    </Link>
-                                    <Link to='/'>
-                                        <i
-                                            className={`${
-                                                styles.icon
-                                            } fa fa-search`}
-                                        />
-                                    </Link>
-                                </div>
-                            </span>
-                        </div>
-                    </div>{' '}
-                    {/* res block end */}
+                        <RestaurantCard name='Takeout' />
+                        <RestaurantCard name='Chillox' />
+                        <RestaurantCard name='Mr. Manik' />
+                    </div> {/* res block end */}
                     {/*Sidebar */}
-                    <div style={{ float: 'left', width: 230 }}>
+                    <div style={{ float: 'left', width: 260 }}>
                         <div className={styles.nearbyBlock}>
                             <p>Nearby Restaurants</p>
                             <div className={styles.nearby}>
@@ -463,11 +297,13 @@ class SearchResult extends Component {
                                 <h5>Mumins Foods</h5>
                                 <h6>Mirpur</h6>
                             </div>
-                            <div style={{ clear: 'both' }}></div>
+                            <div style={{ clear: 'both' }} />
                         </div>
-                    </div> {/* end of sidebar container */}
+                    </div>{' '}
+                    {/* end of sidebar container */}
                     <div style={{ clear: 'both' }} />
-                </div> {/* end of main content block */}
+                </div>{' '}
+                {/* end of main content block */}
                 <div style={{ clear: 'both' }} />
             </div> /* End of container div */
         );
