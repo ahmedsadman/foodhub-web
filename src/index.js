2c5ec7ad0d9541198a8b46e58b70eb22c4e94345
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2019-04-24 12:31:51 +0600

    persist redux state on refresh (redux-persist)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,27 @@ import { Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import logger from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 
 import App from './App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, logger));
+const persistConfig = {
+    key: 'foodhub',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, {}, applyMiddleware(ReduxThunk, logger));
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.querySelector('#root')
 );
