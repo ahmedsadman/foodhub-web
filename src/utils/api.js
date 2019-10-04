1b3e9a86712e36cbac57cd8c8854a7500b4b1c24
Ahmed Sadman <ahmedsadman.211@gmail.com>
2019-10-04 09:58:10 +0600

    switch to build server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,17 @@
-const baseURL = 'http://localhost:3000';
+const baseURL = 'https://foodhub211.herokuapp.com';
 
 export const api = {
-    createBlogPost: `${baseURL}/blog/create`,
-    getBlogList: `${baseURL}/blog`,
-    searchRestaurant: `${baseURL}/restaurant/search`,
-    createRestaurant: `${baseURL}/admin/restaurants/create`,
-    deleteRestaurant: `${baseURL}/admin/restaurants/delete`,
-    reviewRestaurant: (id) => `${baseURL}/restaurant/review/${id}`,
-    placeOrder: (id) => `${baseURL}/restaurant/order/${id}`,
-    orderHistory: `${baseURL}/restaurant/order`,
-    userRestaurant: `${baseURL}/restaurant/user`,
-    updateRestaurant: (id) => `${baseURL}/admin/restaurants/update/${id}`,
-    restaurantDetails: `${baseURL}/restaurant/details`,
-    login: `${baseURL}/auth/login`,
-    register: `${baseURL}/auth/register`
+	createBlogPost: `${baseURL}/blog/create`,
+	getBlogList: `${baseURL}/blog`,
+	searchRestaurant: `${baseURL}/restaurant/search`,
+	createRestaurant: `${baseURL}/admin/restaurants/create`,
+	deleteRestaurant: `${baseURL}/admin/restaurants/delete`,
+	reviewRestaurant: id => `${baseURL}/restaurant/review/${id}`,
+	placeOrder: id => `${baseURL}/restaurant/order/${id}`,
+	orderHistory: `${baseURL}/restaurant/order`,
+	userRestaurant: `${baseURL}/restaurant/user`,
+	updateRestaurant: id => `${baseURL}/admin/restaurants/update/${id}`,
+	restaurantDetails: `${baseURL}/restaurant/details`,
+	login: `${baseURL}/auth/login`,
+	register: `${baseURL}/auth/register`
 };
