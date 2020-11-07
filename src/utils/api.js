2788f3a9939f3b693af33471f9fa8098424b294f
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-11-07 15:50:51 +0600

    changed server url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://foodhub211.herokuapp.com';
+const baseURL = 'https://foodhub-api.herokuapp.com';
 
 export const api = {
 	createBlogPost: `${baseURL}/blog/create`,
@@ -6,12 +6,12 @@ export const api = {
 	searchRestaurant: `${baseURL}/restaurant/search`,
 	createRestaurant: `${baseURL}/admin/restaurants/create`,
 	deleteRestaurant: `${baseURL}/admin/restaurants/delete`,
-	reviewRestaurant: id => `${baseURL}/restaurant/review/${id}`,
-	placeOrder: id => `${baseURL}/restaurant/order/${id}`,
+	reviewRestaurant: (id) => `${baseURL}/restaurant/review/${id}`,
+	placeOrder: (id) => `${baseURL}/restaurant/order/${id}`,
 	orderHistory: `${baseURL}/restaurant/order`,
 	userRestaurant: `${baseURL}/restaurant/user`,
-	updateRestaurant: id => `${baseURL}/admin/restaurants/update/${id}`,
+	updateRestaurant: (id) => `${baseURL}/admin/restaurants/update/${id}`,
 	restaurantDetails: `${baseURL}/restaurant/details`,
 	login: `${baseURL}/auth/login`,
-	register: `${baseURL}/auth/register`
+	register: `${baseURL}/auth/register`,
 };
